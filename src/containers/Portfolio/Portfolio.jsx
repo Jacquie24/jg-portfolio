79f98c3b894804de11d91653bd2e8cf95e5b421a
Jacquie24 <jgcaption@yahoo.com>
2021-02-19 07:49:49 -0500

    Changed hard code to useState and moved over to project.jsx

--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -1,31 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Project from "../../components/Project/Project";
 
-const projectOne = [
-    {
-        image: "http://placekitten.com/g/200/300",
-        title: "Kitten project",
-        description: "This is my best kitty",
-        link: "https://www.github.com"
-
-},
-{
-    image: "http://placekitten.com/g/200/300",
-        title: "Dog project",
-        description: "This is my best puppy",
-        link: "https://www.google.com"
-}
-]
 
 const Portfolio = () => {
+
+
     return (
         <div className="container">
 
-            <h1>This is my Portfolio page</h1>
-            <Project {...projectOne[0]} />
-            <Project {...projectOne[1]} />
+            <h1 className="text-center">This is my Portfolio page</h1>
+            <Project  />
+            <Project  />
+
+            
         </div>
     );
+    
 };
 
 export default Portfolio;
