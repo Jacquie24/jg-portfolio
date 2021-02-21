6ab0e7091d4483a6bb44f24e8e8f77ace2daebc1
Jacquie24 <jgcaption@yahoo.com>
2021-02-21 07:41:46 -0500

    Added background image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import Contact from "./containers/Contact/Contact";
 import Portfolio from "./containers/Portfolio/Portfolio";
+import "./App.css";
 
 
 
@@ -16,8 +17,8 @@ function App() {
           <Route path="/portfolio" component={Portfolio} />
         </Switch>
       </Router>
+      </div>
 
-    </div>
   );
 }
 
