fca7792837b215077a60a0d7e96f15ac8ba6b2b7
Jacquie24 <jgcaption@yahoo.com>
2021-02-21 17:08:33 -0500

    Added specific project info to project cards and styled them

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -12,11 +12,11 @@ const Project = (props) => {
 
             <div className="card" style={{width: "18rem"}}>
 
-  <img src={props.image} className="card-img-top" alt={props.title}></img>
+  <img src={props.src} className="card-img-top" alt={props.title}></img>
   <div className="card-body">
     <h5 className="card-title">{props.title}</h5>
     <p className="card-text">{props.description}</p>
-    <a href={props.link}>Go somewhere</a>
+    <a href={props.link}>Live Link</a>       <a className="float-right" href={props.github}>GitHub repo</a>
   </div>
 </div>
 
--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -1,50 +1,57 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Project from "../../components/Project/Project";
 import Wrapper from '../../Wrapper/Wrapper';
-
+import litPic from "../../imgs/Literate-Listener.png";
+import burgerPic from "../../imgs/eat-da-burger-pic.png";
+import birdPic from "../../imgs/bird brains cover pic.png";
+import empPic from "../../imgs/employee-directory.png";
+import BudgetPic from "../../imgs/budget-tracker.png";
+import dayPic from "../../imgs/day-planner.png";
 
 const Portfolio = () => {
 
     const [projects, setProjects] = useState([
+
         {
-            image: "http://placekitten.com/g/200/300",
-            title: "Kitten project",
-            description: "This is my best kitty",
-            link: "https://www.github.com"
-    
-    },
-    {
-        image: "http://placekitten.com/g/200/300",
-            title: "Dog project",
-            description: "This is my best puppy",
-            link: "https://www.google.com"
-    },
-    {
-        image: "http://placekitten.com/g/200/300",
-        title: "Kitten project",
-        description: "This is my best kitty",
-        link: "https://www.github.com"
+            src: litPic,
+            title: "Literate Listener",
+            description: "Group Project: Search for a book by author or title, find related podcasts and play on the site.",
+            link: "https://jacquie24.github.io/Literate-Listener/"
 
-},
-{
-    image: "http://placekitten.com/g/200/300",
-        title: "Dog project",
-        description: "This is my best puppy",
-        link: "https://www.google.com"
-},
-{
-    image: "http://placekitten.com/g/200/300",
-    title: "Kitten project",
-    description: "This is my best kitty",
-    link: "https://www.github.com"
+        },
 
-},
-{
-image: "http://placekitten.com/g/200/300",
-    title: "Dog project",
-    description: "This is my best puppy",
-    link: "https://www.google.com"
-}
+        {
+            src: burgerPic,
+            title: "Eat Da Burger",
+            description: "Solo Project: User can add burger to the list, choose to devour it, or delete the burger.",
+            link: "https://shielded-falls-19006.herokuapp.com/"
+        },
+        {
+            src: birdPic,
+            title: "Bird Brains",
+            description: "Group Project: Bird watching app that allows users to upload photos of sightings and earn points.",
+            link: "https://bird-brains.herokuapp.com/"
+
+        },
+        {
+            src: empPic,
+            title: "Employee Directory",
+            description: "Solo Project: Table of employees, where users can sort alphabetically or search by name.",
+            link: "https://jacquie24.github.io/employee-directory/"
+        },
+        {
+            src: BudgetPic,
+            title: "Budget Tracker",
+            description: "Solo Project: Users can track Income and Expenditures online and offline",
+            link: "https://vast-bayou-13467.herokuapp.com/"
+
+        },
+        {
+            src: dayPic,
+            title: "Day Planner",
+            description: "Solo Project: A simple calendar app for scheduling the work day",
+            link: "https://jacquie24.github.io/day-planner/"
+        }
     ]);
 
 
@@ -54,14 +61,14 @@ image: "http://placekitten.com/g/200/300",
 
             <h1 className="text-center">Portfolio</h1>
             <Wrapper>
-            {projects.map((project) => (            
-            <Project  {...project}/>
-            ))};
+                {projects.map((project, index) => (
+                    <Project  {...project} key={index} />
+                ))};
 
 </Wrapper>
         </div>
     );
-    
+
 };
 
 export default Portfolio;
