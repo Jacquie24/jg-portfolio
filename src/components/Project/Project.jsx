66a0c3d6353b13c148545c2ed4c138e4f9dc2ee3
Jacquie24 <jgcaption@yahoo.com>
2021-02-21 07:43:48 -0500

    Scaled out basics for header and project layout

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -3,37 +3,22 @@ import React, { useState } from 'react';
 
 
 
-const Project = () => {
+const Project = (props) => {
 
-    const [projects, setProjects] = useState([
-        {
-            image: "http://placekitten.com/g/200/300",
-            title: "Kitten project",
-            description: "This is my best kitty",
-            link: "https://www.github.com"
-    
-    },
-    {
-        image: "http://placekitten.com/g/200/300",
-            title: "Dog project",
-            description: "This is my best puppy",
-            link: "https://www.google.com"
-    }
-    ]);
 
     return (
         <div>
-                            {projects.map((project) => (
+
             <div className="card" style={{width: "18rem"}}>
 
-  <img src={project.image} className="card-img-top" alt={project.title}></img>
+  <img src={props.image} className="card-img-top" alt={props.title}></img>
   <div className="card-body">
-    <h5 className="card-title">{project.title}</h5>
-    <p className="card-text">{project.description}</p>
-    <a href={project.link}>Go somewhere</a>
+    <h5 className="card-title">{props.title}</h5>
+    <p className="card-text">{props.description}</p>
+    <a href={props.link}>Go somewhere</a>
   </div>
 </div>
-  ))};
+
 
 </div>
     );
--- src/containers/Portfolio/Portfolio.jsx
+++ src/containers/Portfolio/Portfolio.jsx
@@ -4,13 +4,31 @@ import Project from "../../components/Project/Project";
 
 const Portfolio = () => {
 
+    const [projects, setProjects] = useState([
+        {
+            image: "http://placekitten.com/g/200/300",
+            title: "Kitten project",
+            description: "This is my best kitty",
+            link: "https://www.github.com"
+    
+    },
+    {
+        image: "http://placekitten.com/g/200/300",
+            title: "Dog project",
+            description: "This is my best puppy",
+            link: "https://www.google.com"
+    }
+    ]);
+
+
 
     return (
         <div className="container">
 
             <h1 className="text-center">This is my Portfolio page</h1>
-            <Project  />
-            <Project  />
+            {projects.map((project) => (            
+            <Project  {...project}/>
+            ))};
 
             
         </div>
