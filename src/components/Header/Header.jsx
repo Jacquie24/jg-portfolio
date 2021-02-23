d7fe94874275271740eb930d945079fcbc586200
Jacquie24 <jgcaption@yahoo.com>
2021-02-22 21:54:54 -0500

    Added Link functionality for paths

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import "./Header.css";
+import { HashRouter, Link } from "react-router-dom";
 
 const Header = () => {
     return (
+      <HashRouter>
         <div>
             <nav className="navbar navbar-expand-lg">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Jacquie Gutierrez</a>
+    <Link className="navbar-brand" to="/">Jacquie Gutierrez</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/#/portfolio">Portfolio</a>
+          <Link className="nav-link active" aria-current="page" to="/portfolio">Portfolio</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/#/contact">Contact</a>
+          <Link className="nav-link" to="/contact">Contact</Link>
         </li>
 
 
@@ -26,6 +28,7 @@ const Header = () => {
   </div>
 </nav>
         </div>
+        </HashRouter>
     );
 };
 
