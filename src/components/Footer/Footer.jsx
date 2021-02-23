c23519bb53c548e5ca89050fdef90f7073222eb7
Jacquie24 <jgcaption@yahoo.com>
2021-02-22 20:27:37 -0500

    Styled footer and navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import "./Footer.css";
 
 const Footer = () => {
     return (
         <div>
-            <nav className="navbar fixed-bottom">
-  <div className="container-fluid">
-    <p className="navbar-brand">Jacquie Gutierrez 2021</p>
+            {/* <nav className="navbar fixed-bottom"> */}
+  {/* <div className="container-fluid">
+    <p className="navbar-brand">Jacquie Gutierrez 2021</p> */}
+    <footer className="footer">
+    <div className="two-toned-footer-color"></div>
+    <p className="text-muted text-muted-footer text-center">
+      &copy; Copyright 2021 Jacquie Gutierrez
+    </p>
+  </footer>
   </div>
-</nav>
-        </div>
+
     );
 };
 
